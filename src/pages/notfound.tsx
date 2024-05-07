d7a63c899b2be3be989beabb0026fbf1a2a0d0ba
saadkmss <kamousssaad35@gmail.com>
2024-05-07 12:29:15 +0100

    first commit

---
+++ src/pages/notfound.tsx
@@ -0,0 +1,12 @@
+import { buttonVariants } from '../components/ui/button';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+    return (
+        <div className='flex flex-col gab-2'>
+            <div>404 NOT FOUND</div>
+            <Link className={buttonVariants({ variant: "outline" })} to="/">Click here to go back to the HomePage</Link>
+        </div>
+    );
+}
