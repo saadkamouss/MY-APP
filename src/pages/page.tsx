f145f42af01938e5973e3a3a5d2dfdd834e9921e
Med Amine <ma.elazzab@gmail.com>
2024-05-13 14:16:56 +0100

    refactor: clean code

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-export default function HomePage(){
-    return (
-        <div>
-            <h1>Home page</h1>
-        </div>
-    );
+export default function HomePage() {
+  return (
+    <div>
+
+
+
+      <h1>Home page</h1>
+
+
+
+
+      
+    </div>
+  );
 }
--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-export default function ProfilePage(){
-    return(
-        <div>
-            <h1>Profile page</h1>
-        </div>
-    
-    );
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>Profile page</h1>
+    </div>
+  );
 }
