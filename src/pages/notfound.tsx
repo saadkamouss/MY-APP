c2f586af64f303aa9949370217c03a0760527817
saadkmss <kamousssaad35@gmail.com>
2024-05-18 00:59:04 +0100

    refactor: clean code

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -7,7 +7,14 @@ export default function NotFound() {
     <div className='flex flex-col gab-2'>
       <div>404 NOT FOUND</div>
       <Link className={buttonVariants({ variant: 'outline' })} to='/'>
+
+
+
+
         Click here to go back to the HomePage
+
+
+        
       </Link>
     </div>
   );
